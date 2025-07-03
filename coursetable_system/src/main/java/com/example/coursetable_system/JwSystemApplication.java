45c251112889db7fd8f73f0d04b429844e4d8f5e
qiukanghan <2807318296@qq.com>
2025-07-03 17:22:41 +0800

    Add files via upload

---
+++ coursetable_system/src/main/java/com/example/coursetable_system/JwSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursetable_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwSystemApplication.class, args);
+	}
+
+}
