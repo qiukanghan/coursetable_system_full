d4b367c3444c499e5f8dcac7a2da567b01016f95
qiukanghan <2807318296@qq.com>
2025-07-11 00:24:35 +0800

    Delete coursetable_system/src/main/java/com/example/coursetable_system/repository/CourseInsertRepository.java

--- coursetable_system/src/main/java/com/example/coursetable_system/repository/CourseInsertRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.coursetable_system.repository;
-
-import com.example.coursetable_system.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseInsertRepository extends JpaRepository<Course, String> {
-}
